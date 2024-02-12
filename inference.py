2d8cf68124ccb91263d0b9e640248f519f26ab43
Wonjun <jwj51720@gmail.com>
2024-02-13 02:53:08 +0900

    feat: can do inference

--- inference.py
+++ inference.py
@@ -1,18 +1,40 @@
 import os
 import torch
 import json
-import argparse  # argparse 모듈 임포트
+import argparse
+from tqdm import tqdm
 from modules import *
 
 
 def main(CFG):
     print("**START**")
     model = get_model(CFG, "inference")
-    train_loader, valid_loader = get_loader(CFG)
+    test_loader = get_loader(CFG)
     print("**LOAD DATA COMPLETE**")
-    trainer(CFG, model, train_loader, valid_loader)
+    inference(CFG, model, test_loader)
     print("**MODEL INFERENCE COMPLETE**")
     return 0
+
+
+def inference(CFG, model, test_loader):
+    config = CFG["INFERENCE"]
+    outputs = []
+    progress_bar = tqdm(enumerate(test_loader), total=len(test_loader))
+    with torch.no_grad():
+        for batch_idx, batch in progress_bar:
+            output_sequences = model.generate(
+                input_ids=batch.to(device),
+                max_length=config["MAX_LENGTH"],
+                temperature=config["TEMPERATURE"],
+                top_k=config["TOP_K"],
+                top_p=config["TOP_P"],
+                repetition_penalty=config["REPETITION_PENALTY"],
+                do_sample=config["DO_SAMPLE"],
+                num_return_sequences=config["NUM_RETURN_SEQUENCES"],
+            )
+            outputs.append(output_sequences)
+        preds = extract_answer(CFG, outputs)
+    submission(CFG, preds)
 
 
 if __name__ == "__main__":
@@ -45,6 +67,6 @@ if __name__ == "__main__":
     else:
         print(f"CUDA is not available. Using {device}.")
     config["DEVICE"] = device
-    if not os.path.exists(config['SAVE_PATH']):
-        os.makedirs(config['SAVE_PATH'])
+    if not os.path.exists(config["SAVE_PATH"]):
+        os.makedirs(config["SAVE_PATH"])
     main(config)
